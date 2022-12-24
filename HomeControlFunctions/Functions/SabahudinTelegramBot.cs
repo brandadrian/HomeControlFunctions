a16bd2a873a77a73edeb477a328665a68886aeb0
brandadrian <brandadrian@gmail.com>
2022-12-24 16:11:38 +0100

    feat: Add dynamic responses

--- HomeControlFunctions/Functions/SabahudinTelegramBot.cs
+++ HomeControlFunctions/Functions/SabahudinTelegramBot.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -16,10 +17,12 @@ namespace HomeControlFunctions.Functions
     public class SabahudinTelegramBot
     {
         private readonly TelegramBotClient _botClient;
+        private readonly List<string> _sabahudinTelegramResponses = new List<string>();
 
         public SabahudinTelegramBot(IConfiguration configuration)
         {
             var telegramApiKey = configuration.GetSection("SabahudinTelegramApiKey").Value;
+            _sabahudinTelegramResponses.AddRange(configuration.GetSection("SabahudinTelegramResponses").Get<List<string>>());
             _botClient = new TelegramBotClient(telegramApiKey);
         }
 
@@ -54,7 +57,6 @@ namespace HomeControlFunctions.Functions
                 var firstName = update?.Message?.Chat?.FirstName;
                 var lastName = update?.Message?.Chat?.LastName;
                 var text = update?.Message?.Text;
-
                 log.LogInformation("Request received. FirstName: {FirstName}. LastName: {LastName}. Text: {Text}", firstName, lastName, text);
 
                 if (update.Type != UpdateType.Message)
@@ -75,38 +77,25 @@ namespace HomeControlFunctions.Functions
 
         private string GetSabahudinsAnswer(string? senderFirstName, string? senderLastName, string messageText)
         {
-            var random = new Random();
-            var randomNumer = random.NextInt64(1, 20);
-            var responseName = string.IsNullOrEmpty(senderFirstName) ? senderLastName : (senderFirstName ?? "curaz");
-            var isQuestion = messageText.Contains("?");
-
-            if (isQuestion)
+            try
             {
-                randomNumer = 0;
+                var responseName = string.IsNullOrEmpty(senderFirstName) ? senderLastName : (senderFirstName ?? "curaz");
+
+                _sabahudinTelegramResponses.Add($"{responseName} ash patat?");
+                _sabahudinTelegramResponses.Add($"Ash epfel {responseName}? Bruhe ajne");
+                _sabahudinTelegramResponses.Add($"Frelihe wajnahte {responseName}");
+                _sabahudinTelegramResponses.Add($"Wj isch tinj name showjder? Tje {responseName} rihtik?");
+                _sabahudinTelegramResponses.Add($"oo {responseName} tu bisch ajne gute man");
+
+                var random = new Random();
+                var randomNumer = random.Next(0, _sabahudinTelegramResponses.Count - 1);
+
+                return _sabahudinTelegramResponses[randomNumer];
             }
-
-            return randomNumer switch
+            catch
             {
-                0 => "I nix versteh dojc",
-                1 => "Hau lele i abe rykshmerc",
-                2 => "Prrcc..",
-                3 => $"{responseName} ash patat?",
-                4 => "Pil ehrs",
-                5 => $"Ash epfel {responseName}? Bruhe ajne",
-                6 => "Vom wo chomsch tu?",
-                7 => "Mehtesh?",
-                8 => "Kajn",
-                9 => "Sjer",
-                10 => "Bini im kjhe",
-                11 => "Wan ajne esel isch im stall vom ajne kuh. Tjze dan ajne kuh oder ajne esel?",
-                12 => $"Frelihe wajnahte {responseName}",
-                13 => "Chomt bald santaclos?",
-                14 => "Pushi curaz minj rykc...",
-                15 => $"Wj isch tinj name showjder? Tje {responseName} rihtik?",
-                16 => "I nix versteh",
-                17 => $"oo {responseName} tu bisch ajne gute man",
-                _ => "muss"
-            };
+                return "I nix versteh";
+            }
         }
     }
 }
